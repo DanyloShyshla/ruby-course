abfe07581c9f27c7e27fc3b639c77c0e9f6b0883
DanyloShyshla <dshyshla@gmail.com>
2021-11-19 22:18:44 +0200

    Removed puts

--- codewars/where_is_he.rb
+++ codewars/where_is_he.rb
@@ -1,5 +1,5 @@
 def where_is_he(people_in_line, people_before, people_after)
-  puts [people_in_line - people_before, people_after + 1].min
+  [people_in_line - people_before, people_after + 1].min
 end
 
-where_is_he(5, 2, 3)
+puts where_is_he(5, 2, 3)
